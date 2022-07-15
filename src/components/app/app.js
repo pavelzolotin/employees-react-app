b7bc4bd02311c0890d8e6244b708e6317374dc0f
Pavel Zolotin <zolotinpavel@gmail.com>
2022-07-15 08:52:55 +0500

    add dynamic logic to components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,8 +3,31 @@ import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
-
 import './app.css';
+
+const data = [
+    {
+        name: 'John D.',
+        salary: 1000,
+        increase: false,
+        like: false,
+        id: 1
+    },
+    {
+        name: 'Sam S.',
+        salary: 2500,
+        increase: true,
+        like: false,
+        id: 2
+    },
+    {
+        name: 'Gven M.',
+        salary: 3000,
+        increase: false,
+        like: false,
+        id: 3
+    }
+]
 
 function App() {
     return (
@@ -13,7 +36,7 @@ function App() {
             <div className="search-panel">
                 <SearchPanel/>
                 <AppFilter/>
-                <EmployeesList/>
+                <EmployeesList data={ data }/>
                 <EmployeesAddForm/>
             </div>
         </div>
